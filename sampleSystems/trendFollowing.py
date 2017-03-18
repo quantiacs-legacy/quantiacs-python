7ff88bded20f2fda179e6314b8b06f278ef125e5
Yalei <yaleis@alumni.cmu.edu>
2017-03-17 17:27:00 -0700

    delete  data files and reverse trendFollowing.py changes

--- sampleSystems/trendFollowing.py
+++ sampleSystems/trendFollowing.py
@@ -44,15 +44,12 @@ def mySettings():
     # 'UPS','USB','UTX','V','VZ','WAG','WFC','WMT','XOM']
 
     # Futures Contracts
-    '''
     settings['markets'] = ['CASH', 'F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CC', 'F_CD',
                            'F_CL', 'F_CT', 'F_DX', 'F_EC', 'F_ED', 'F_ES', 'F_FC', 'F_FV', 'F_GC',
                            'F_HG', 'F_HO', 'F_JY', 'F_KC', 'F_LB', 'F_LC', 'F_LN', 'F_MD', 'F_MP',
                            'F_NG', 'F_NQ', 'F_NR', 'F_O', 'F_OJ', 'F_PA', 'F_PL', 'F_RB', 'F_RU',
                            'F_S', 'F_SB', 'F_SF', 'F_SI', 'F_SM', 'F_TU', 'F_TY', 'F_US', 'F_W',
                            'F_XX', 'F_YM']
-    '''
-    settings['markets'] = ['CASH', 'F_AD', 'F_ES', 'F_BO', 'F_BP']
 
     settings['lookback'] = 504
     settings['budget'] = 10**6
